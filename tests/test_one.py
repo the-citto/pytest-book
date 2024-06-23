277f6d077384b695af032954e137231660d217ad
the-citto <alex.citterio@hotmail.co.uk>
2024-06-23 18:28:11 +0100

    done ch01

---
+++ tests/test_one.py
@@ -0,0 +1,9 @@
+"""Test one."""
+
+def test_passing() -> None:
+    """First passing test."""
+    assert (1, 2, 3) == (1, 2, 3)
+
+
+
+
---
+++ tests/test_two.py
@@ -0,0 +1,6 @@
+"""Test two."""
+
+
+def test_failing() -> None:
+    """First failing test."""
+    assert (1, 2, 3) == (3, 2, 1)
